a51dd013d49edb1af1289c0833a9e7cc2e93e35d
testerdotnet <jigartester@yahoo.com>
2015-01-22 11:39:34 -0600

    added first version

---
+++ ConsoleApplication1/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            const string s = "PEACE";
+            string[] words = {"SHRA'VIKA PARIKH"};
+            var intsum = 0;
+            foreach (string value in words)
+            {
+                intsum = 0;
+                 foreach (char c in value)
+                {
+                    //Console.WriteLine(" Char -> " + c.TextToNumber());
+                     intsum += (c == ' ' || c == '\'' ? 0 : c.TextToNumber());
+                }
+                Console.WriteLine(value + " ->" +intsum.ToString());
+            }
+
+            
+        }
+
+       
+    }
+}
---
+++ ConsoleApplication1/TexttoNumber.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication1
+{
+    public static class TexttoNumber
+    {
+        public static int TextToNumber(this char text)
+        {
+            //return text
+            //    .Select(c => c - 'A' + 1)
+            //    .Aggregate((sum, next) => sum * 26 + next);
+            return (text - 'A') + 1;
+        }
+    }
+}
